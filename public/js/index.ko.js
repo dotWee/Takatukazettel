8db1c17a80179021382c043e8c47519ba92ce217
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-11-29 23:35:01 +0100

    First working knockout binding

--- public/js/index.ko.js
+++ public/js/index.ko.js
@@ -12,3 +12,50 @@ function ItemModel(data) {
         self.user(data.user);
     }
 }
+
+function IndexModel() {
+    var self = this;
+
+    self.items = ko.observableArray([]);
+    self.currentItem = ko.observable(new ItemModel());
+
+    // Load items
+    self.refreshList = function (endpointParam = "") {
+
+        // Clear old array
+        self.items.removeAll();
+
+        var xreq = new XMLHttpRequest();
+        xreq.addEventListener("load", function (data) {
+            var itemArray = JSON.parse(data.target.responseText);
+            console.log("Pulled " + itemArray.length + " items");
+
+            // Add each item to local array
+            JSON.parse(data.target.responseText).forEach(function (element) {
+                self.items.push(new ItemModel(element));
+            }, this);
+
+            console.log("Loaded " + self.items().length + " into the view");
+            ko.applyBindings(self);
+        });
+        xreq.open("GET", endpoint + endpointParam);
+        xreq.setRequestHeader("Content-type", "application/json");
+        xreq.send();
+    };
+    
+    self.refreshList();
+
+    self.addItem = function () {
+        console.log("Create new item with values: " + ko.toJSON(self.currentItem));
+
+        var xreq = new XMLHttpRequest();
+        xreq.addEventListener("load", function (data) {
+            self.refreshList();
+        });
+        xreq.open("POST", endpoint);
+        xreq.setRequestHeader("Content-type", "application/json");
+        xreq.send(ko.toJSON(self.currentItem));
+    };
+}
+
+var localModel = new IndexModel();
